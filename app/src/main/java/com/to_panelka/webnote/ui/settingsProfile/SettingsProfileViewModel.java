4740baf24f60e520d813615f0aecfd73d1290bb9
khvatid <hv20027@gmail.com>
2021-10-13 22:44:47 +0300

    Добавлена заготовка под настройки профиля

---
+++ app/src/main/java/com/to_panelka/webnote/ui/settingsProfile/SettingsProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.to_panelka.webnote.ui.settingsProfile;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsProfileViewModel extends ViewModel {
+  // TODO: Implement the ViewModel
+}
